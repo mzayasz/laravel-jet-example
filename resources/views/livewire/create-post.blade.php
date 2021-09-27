9577dea3bd875249c478168d21b9dd8c762d45c3
MartinZayasZ <zayasmartin70@gmail.com>
2021-09-27 13:28:47 -0700

    tiempos de carga livewire

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -25,9 +25,12 @@
             <x-jet-secondary-button wire:click="$set('open', false)">
                 Cancelar
             </x-jet-secondary-button>
-            <x-jet-danger-button wire:click="save">
+            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save" class="disabled:opacity-25">
                 Crear
             </x-jet-danger-button>
+
+            <span wire:loading wire:target="save">Cargando ... </span>
+
         </x-slot>
     </x-jet-dialog-modal>
 
